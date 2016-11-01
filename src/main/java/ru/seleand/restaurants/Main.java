241082a6d15a2c11ecb6f9242e3f8698eba7b73f
seleand <seleand_rabota@mail.ru>
2016-11-01 09:56:22 +0300

    Initial commit

---
+++ src/main/java/ru/seleand/restaurants/Main.java
@@ -0,0 +1,10 @@
+package ru.seleand.restaurants;
+
+/**
+ * Created by Asus on 31.10.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Everything is allright!");
+    }
+}
