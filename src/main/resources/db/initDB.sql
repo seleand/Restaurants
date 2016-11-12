39a43a176e2e0ca419b81db1fd4cf3e115e3108a
seleand <seleand_rabota@mail.ru>
2016-11-12 14:34:09 +0300

    postggresql properties, initDb, populate db

---
+++ src/main/resources/db/initDB.sql
@@ -0,0 +1,42 @@
+DROP TABLE IF EXISTS user_roles;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS restaurants;
+DROP TABLE IF EXISTS dishes;
+DROP SEQUENCE IF EXISTS global_seq;
+
+CREATE SEQUENCE global_seq START 100000;
+
+CREATE TABLE users
+(
+  id         INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  name       VARCHAR NOT NULL,
+  email      VARCHAR NOT NULL,
+  password   VARCHAR NOT NULL,
+  registered TIMESTAMP DEFAULT now(),
+  enabled    BOOL DEFAULT TRUE
+);
+CREATE UNIQUE INDEX users_unique_email_idx ON users (email);
+
+CREATE TABLE user_roles
+(
+  user_id INTEGER NOT NULL,
+  role    VARCHAR,
+  CONSTRAINT user_roles_idx UNIQUE (user_id, role),
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE TABLE restaurants
+(
+  id         INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  name       VARCHAR NOT NULL
+);
+
+CREATE TABLE dishes
+(
+  id         INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  description       VARCHAR NOT NULL,
+  price INTEGER NOT NULL,
+  restaurant_id INTEGER NOT NULL,
+  FOREIGN KEY (restaurant_id) REFERENCES restaurants (id) ON DELETE CASCADE
+);
+
---
+++ src/main/resources/db/populateDB.sql
@@ -0,0 +1,30 @@
+DELETE FROM user_roles;
+DELETE FROM users;
+DELETE FROM restaurants;
+DELETE FROM dishes;
+ALTER SEQUENCE global_seq RESTART WITH 100000;
+
+INSERT INTO users (name, email, password)
+VALUES ('User', 'user@yandex.ru', 'password');
+
+INSERT INTO users (name, email, password)
+VALUES ('Admin', 'admin@gmail.com', 'admin');
+
+INSERT INTO user_roles (role, user_id) VALUES
+  ('ROLE_USER', 100000),
+  ('ROLE_ADMIN', 100001);
+
+INSERT INTO restaurants (name)
+VALUES ('restaurant 1');
+
+INSERT INTO restaurants (name)
+VALUES ('restaurant 2');
+
+INSERT INTO dishes (description, price, restaurant_id)
+VALUES
+  ('dish 1 rest1', 5000, 100002),
+  ('dish 2 rest1', 6515, 100002),
+  ('dish 3 rest1', 8005, 100002),
+  ('dish 1 rest2', 7000, 100003),
+  ('dish 2 rest2', 5700, 100003),
+  ('dish 3 rest3', 6000, 100003);
