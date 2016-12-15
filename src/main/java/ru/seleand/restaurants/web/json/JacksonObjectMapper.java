fce80ca714e4f31dd1eeef0d6a934cccc5fbc539
seleand <seleand_rabota@mail.ru>
2016-12-15 18:55:57 +0300

    jackson jsr310

--- src/main/java/ru/seleand/restaurants/web/json/JacksonObjectMapper.java
+++ src/main/java/ru/seleand/restaurants/web/json/JacksonObjectMapper.java
@@ -4,8 +4,10 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -31,10 +33,14 @@ public class JacksonObjectMapper extends ObjectMapper {
     private JacksonObjectMapper() {
         registerModule(new Hibernate5Module());
 
+/*
         SimpleModule customModule = new SimpleModule("customModule");
         customModule.addSerializer(new JsonLocalDateConverter.LocalDateSerializer());
         customModule.addDeserializer(LocalDate.class, new JsonLocalDateConverter.LocalDateDeserializer());
         registerModule(customModule);
+*/
+        registerModule(new JavaTimeModule());
+        configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
 
         setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.NONE);
         setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
