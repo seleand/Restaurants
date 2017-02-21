11e951725531ecbc83c7b1d3e82d2e42bbcabf4c
seleand <seleand_rabota@mail.ru>
2017-02-21 15:57:44 +0300

    fix BaseEntity

--- src/main/java/ru/seleand/restaurants/model/BaseEntity.java
+++ src/main/java/ru/seleand/restaurants/model/BaseEntity.java
@@ -37,7 +37,7 @@ public class BaseEntity implements Persistable<Integer> {
 
     @Override
     public boolean isNew() {
-        return (this.id == null);
+        return (getId() == null);
     }
 
     @Override
