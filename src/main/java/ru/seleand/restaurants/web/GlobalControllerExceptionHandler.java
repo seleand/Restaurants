30b5c872606a575504c65ae994479022248b2dea
seleand <seleand_rabota@mail.ru>
2017-03-15 14:57:43 +0300

    global exception

---
+++ src/main/java/ru/seleand/restaurants/web/GlobalControllerExceptionHandler.java
@@ -0,0 +1,32 @@
+package ru.seleand.restaurants.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+import ru.seleand.restaurants.AuthorizedUser;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class GlobalControllerExceptionHandler {
+    private static final Logger LOG = LoggerFactory.getLogger(GlobalControllerExceptionHandler.class);
+
+    @ExceptionHandler(Exception.class)
+    @Order(Ordered.LOWEST_PRECEDENCE)
+    ModelAndView defaultErrorHandler(HttpServletRequest req, Exception e) throws Exception {
+        LOG.error("Exception at request " + req.getRequestURL(), e);
+        ModelAndView mav = new ModelAndView("exception/exception");
+        mav.addObject("exception", e);
+
+        // Interceptor is not invoked, put userTo
+        AuthorizedUser authorizedUser = AuthorizedUser.safeGet();
+        if (authorizedUser != null) {
+            mav.addObject("userTo", authorizedUser.getUserTo());
+        }
+        return mav;
+    }
+}
